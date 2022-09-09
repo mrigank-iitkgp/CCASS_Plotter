bb70a3fbcc48b4b923d603b4269ebcf3491dc267
mrigank-iitkgp <mrigank432@gmail.com>
2022-09-09 20:22:27 +0530

    fast API

---
+++ backend/api/main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from data_extraction.trendPlot import TrendPlotter
+from data_extraction.data_manipulation import ShareholdingData
+import json
+
+app = FastAPI()
+
+@app.get("/api/gettopkshareholders")
+def get_top_k_shareholders(stockCode , startDate , endDate , k):
+    result = TrendPlotter.getPlotData(stockCode , ShareholdingData() , startDate , endDate , k)
+    return result
